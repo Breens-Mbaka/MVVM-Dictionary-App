3753ebf0a21feebafa6b9c34930ddf15dd3ef7f5
Breens Mbaka <breens.mbaka@dlight.com>
2023-01-04 07:38:15 +0300

    Provide a dispatchers module

---
+++ app/src/main/java/com/breens/mvvmdictionaryapp/di/DictionaryAppDispatchers.kt
@@ -0,0 +1,12 @@
+package com.breens.mvvmdictionaryapp.di
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val dictionaryAppDispatchers: DictionaryAppDispatchers)
+
+enum class DictionaryAppDispatchers {
+  IO
+}
