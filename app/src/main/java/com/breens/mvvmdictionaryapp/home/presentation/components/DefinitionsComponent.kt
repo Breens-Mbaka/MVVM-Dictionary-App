b93bc8d8c4eb354fd795f36789e472fd460952dd
Breens Mbaka <breens.mbaka@dlight.com>
2023-01-17 06:52:12 +0300

    Add spacing between the definitions

--- app/src/main/java/com/breens/mvvmdictionaryapp/home/presentation/components/DefinitionsComponent.kt
+++ app/src/main/java/com/breens/mvvmdictionaryapp/home/presentation/components/DefinitionsComponent.kt
@@ -1,14 +1,17 @@
 package com.breens.mvvmdictionaryapp.home.presentation.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import com.breens.mvvmdictionaryapp.R
 import com.breens.mvvmdictionaryapp.home.data.remote.DefinitionPresentationModel
 
@@ -42,7 +45,8 @@ fun DefinitionsComponent(
                     ) {
                         append(meaning.definition ?: "")
                     }
-                }
+                },
+                modifier = Modifier.padding(top = 5.dp)
             )
         }
     }
